f757fd6400d343be42a9c16094303c9639365286
Unai de la Mata <unaimax09@gmail.com>
2023-11-16 18:17:14 +0100

    commit

--- src/main/java/es/uca/iw/views/helloworld/HelloWorldView.java
+++ src/main/java/es/uca/iw/views/helloworld/HelloWorldView.java
@@ -2,8 +2,9 @@ package es.uca.iw.views.helloworld;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -13,12 +14,14 @@ import es.uca.iw.views.MainLayout;
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
-public class HelloWorldView extends HorizontalLayout {
+public class HelloWorldView extends VerticalLayout {
 
     private TextField name;
     private Button sayHello;
 
     public HelloWorldView() {
+        H1 welcomeText = new H1("UFJMOVIL");
+        H1 welcomeText2 = new H1("Tu compañia telefónica de confianza");
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
@@ -27,9 +30,12 @@ public class HelloWorldView extends HorizontalLayout {
         sayHello.addClickShortcut(Key.ENTER);
 
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        //setVerticalComponentAlignment(Alignment.END, name, sayHello);
 
+        add(welcomeText);
+        add(welcomeText2);
         add(name, sayHello);
+        
     }
 
 }
