2194fa185b05fe7a7a5bd0e6725b10d0f87915da
jonasnow <125695440+jonasnow@users.noreply.github.com>
2024-01-03 13:37:50 +0100

    Corrección de errores de tipo

--- src/main/java/es/uca/iw/repositories/ClienteRepository.java
+++ src/main/java/es/uca/iw/repositories/ClienteRepository.java
@@ -1,6 +1,9 @@
 package es.uca.iw.repositories;
 
 import es.uca.iw.domain.Cliente;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
