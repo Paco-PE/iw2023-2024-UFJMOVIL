f449dcf9088a690bbd54450211f3b0f884b470d5
jonasnow <125695440+jonasnow@users.noreply.github.com>
2024-01-03 23:01:54 +0100

    Revert "Metodo contratar"
    This reverts commit 0ce9eb49b12e4db8792cfee3dfe8c4e7183cb1e4.

--- src/main/java/es/uca/iw/services/ContratoService.java
+++ src/main/java/es/uca/iw/services/ContratoService.java
@@ -3,50 +3,20 @@ package es.uca.iw.services;
 import org.springframework.stereotype.Service;
 
 import es.uca.iw.repositories.ContratoRepository;
-import es.uca.iw.repositories.ServicioRepository;
-import jakarta.transaction.Transactional;
-import es.uca.iw.repositories.ClienteRepository;
 
 import es.uca.iw.domain.Contrato;
 
 import java.util.List;
-import java.util.UUID;
-
-import es.uca.iw.domain.Servicio;
-import es.uca.iw.domain.Cliente;
-import java.util.Date;
 
 @Service
 public class ContratoService {
-    private final ContratoRepository contratoRepository;
-    private final ServicioRepository servicioRepository;
-    private final ClienteRepository clienteRepository;
+    private final ContratoRepository repository;
 
-    public ContratoService(ContratoRepository contratoRepository, ServicioRepository servicioRepository, ClienteRepository clienteRepository) {
-        this.contratoRepository = contratoRepository;
-        this.servicioRepository = servicioRepository;
-        this.clienteRepository = clienteRepository;
+    public ContratoService(ContratoRepository repository){
+        this.repository = repository;
     }
 
     public List<Contrato> findAll(){
-        return contratoRepository.findAll();
-    }
-
-
-    @Transactional
-    public Contrato contratar(UUID servicioId, UUID clienteId, Date fechaInicio, Date fechaFin, float costeMensual) {
-
-        Servicio servicio = servicioRepository.findById(servicioId).orElseThrow(() -> new RuntimeException("Servicio no encontrado con ID: " + servicioId));
-        Cliente cliente = clienteRepository.findById(clienteId).orElseThrow(() -> new RuntimeException("Cliente no encontrado con ID: " + clienteId));
-
-        Contrato contrato = new Contrato();
-        contrato.setServicio(servicio);
-        contrato.setCliente(cliente);
-        contrato.setFechaInicio(fechaInicio);
-        contrato.setFechaFin(fechaFin);
-        contrato.setCosteMensual(costeMensual);
-
-        return contratoRepository.save(contrato);
+        return repository.findAll();
     }
 }
-
